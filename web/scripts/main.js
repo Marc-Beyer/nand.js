2060bb82762bfca78b8485b125c2a01feaa47e4b
Marc Beyer <marc.beyer.dev@gmail.com>
2021-01-02 23:27:25 +0100

    Made UI dragable

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,23 +1,23 @@
 var mainCircuit = new Circuit({ x: 10, y: 10 });
-mainCircuit.gates.unshift(new Lable_Gate({ x: 50, y: 50 }, "Inputs"));
-mainCircuit.gates.unshift(new CONST_HIGH_Gate({ x: 50, y: 150 }));
-mainCircuit.gates.unshift(new CONST_LOW_Gate({ x: 50, y: 250 }));
-mainCircuit.gates.unshift(new Switch_Gate({ x: 50, y: 350 }));
-mainCircuit.gates.unshift(new Lable_Gate({ x: 250, y: 50 }, "1-Input gates"));
-mainCircuit.gates.unshift(new Buffer_Gate({ x: 250, y: 150 }));
-mainCircuit.gates.unshift(new NOT_Gate({ x: 250, y: 250 }));
-mainCircuit.gates.unshift(new Lable_Gate({ x: 450, y: 50 }, "Conjunction\nand Disjunction"));
-mainCircuit.gates.unshift(new AND_Gate({ x: 450, y: 150 }));
-mainCircuit.gates.unshift(new OR_Gate({ x: 450, y: 250 }));
-mainCircuit.gates.unshift(new Lable_Gate({ x: 650, y: 50 }, "Alternative denial\nand Joint denial"));
-mainCircuit.gates.unshift(new NAND_Gate({ x: 650, y: 150 }));
-mainCircuit.gates.unshift(new NOR_Gate({ x: 650, y: 250 }));
-mainCircuit.gates.unshift(new Lable_Gate({ x: 900, y: 50 }, "Exclusive or\nand Biconditional"));
-mainCircuit.gates.unshift(new XOR_Gate({ x: 900, y: 150 }));
-mainCircuit.gates.unshift(new XNOR_Gate({ x: 900, y: 250 }));
-mainCircuit.gates.unshift(new Lable_Gate({ x: 1150, y: 50 }, "Outputs"));
-mainCircuit.gates.unshift(new Lamp_Gate({ x: 1150, y: 150 }));
-mainCircuit.gates.unshift(new Display_Gate({ x: 1150, y: 250 }));
+mainCircuit.gates.unshift(new Lable_Gate({ x: 350, y: 50 }, "Inputs"));
+mainCircuit.gates.unshift(new CONST_HIGH_Gate({ x: 350, y: 150 }));
+mainCircuit.gates.unshift(new CONST_LOW_Gate({ x: 350, y: 250 }));
+mainCircuit.gates.unshift(new Switch_Gate({ x: 350, y: 350 }));
+mainCircuit.gates.unshift(new Lable_Gate({ x: 550, y: 50 }, "1-Input gates"));
+mainCircuit.gates.unshift(new Buffer_Gate({ x: 550, y: 150 }));
+mainCircuit.gates.unshift(new NOT_Gate({ x: 550, y: 250 }));
+mainCircuit.gates.unshift(new Lable_Gate({ x: 750, y: 50 }, "Conjunction\nand Disjunction"));
+mainCircuit.gates.unshift(new AND_Gate({ x: 750, y: 150 }));
+mainCircuit.gates.unshift(new OR_Gate({ x: 750, y: 250 }));
+mainCircuit.gates.unshift(new Lable_Gate({ x: 950, y: 50 }, "Alternative denial\nand Joint denial"));
+mainCircuit.gates.unshift(new NAND_Gate({ x: 950, y: 150 }));
+mainCircuit.gates.unshift(new NOR_Gate({ x: 950, y: 250 }));
+mainCircuit.gates.unshift(new Lable_Gate({ x: 1200, y: 50 }, "Exclusive or\nand Biconditional"));
+mainCircuit.gates.unshift(new XOR_Gate({ x: 1200, y: 150 }));
+mainCircuit.gates.unshift(new XNOR_Gate({ x: 1200, y: 250 }));
+mainCircuit.gates.unshift(new Lable_Gate({ x: 1450, y: 50 }, "Outputs"));
+mainCircuit.gates.unshift(new Lamp_Gate({ x: 1450, y: 150 }));
+mainCircuit.gates.unshift(new Display_Gate({ x: 1450, y: 250 }));
 //mainCircuit.gates[0].connections.concat({gate: mainCircuit.gates[1], outputNr: 0, inputNr: 0});
 mainCircuit.refrashCanvas();
 var dropContainer = document.getElementsByClassName("drop-container");
@@ -98,5 +98,20 @@ for (var index = 0; index < addButtons.length; index++) {
 var errorContainer = document.getElementById("error-container");
 errorContainer.getElementsByTagName("BUTTON")[0].addEventListener("click", function () {
     errorContainer.className = "hidden";
+});
+var menu = document.getElementById("menu");
+var dragContainer = document.getElementById("drag-container");
+var isDraggingMenu = false;
+dragContainer.addEventListener("mousedown", function (e) {
+    isDraggingMenu = true;
+});
+document.addEventListener("mouseup", function (e) {
+    isDraggingMenu = false;
+});
+document.addEventListener("mousemove", function (e) {
+    if (!isDraggingMenu)
+        return;
+    menu.style.left = e.clientX - dragContainer.getBoundingClientRect().width / 2 + "px";
+    menu.style.top = e.clientY - dragContainer.getBoundingClientRect().height / 2 + "px";
 });
 //# sourceMappingURL=main.js.map
--- web/scripts/main.ts
+++ web/scripts/main.ts
@@ -1,30 +1,30 @@
 
 var mainCircuit = new Circuit({x: 10, y: 10});
 
-mainCircuit.gates.unshift(new Lable_Gate({x: 50, y: 50}, "Inputs"));
-mainCircuit.gates.unshift(new CONST_HIGH_Gate({x: 50, y: 150}));
-mainCircuit.gates.unshift(new CONST_LOW_Gate({x: 50, y: 250}));
-mainCircuit.gates.unshift(new Switch_Gate({x: 50, y: 350}));
+mainCircuit.gates.unshift(new Lable_Gate({x: 350, y: 50}, "Inputs"));
+mainCircuit.gates.unshift(new CONST_HIGH_Gate({x: 350, y: 150}));
+mainCircuit.gates.unshift(new CONST_LOW_Gate({x: 350, y: 250}));
+mainCircuit.gates.unshift(new Switch_Gate({x: 350, y: 350}));
 
-mainCircuit.gates.unshift(new Lable_Gate({x: 250, y: 50}, "1-Input gates"));
-mainCircuit.gates.unshift(new Buffer_Gate({x: 250, y: 150}));
-mainCircuit.gates.unshift(new NOT_Gate({x: 250, y: 250}));
+mainCircuit.gates.unshift(new Lable_Gate({x: 550, y: 50}, "1-Input gates"));
+mainCircuit.gates.unshift(new Buffer_Gate({x: 550, y: 150}));
+mainCircuit.gates.unshift(new NOT_Gate({x: 550, y: 250}));
 
-mainCircuit.gates.unshift(new Lable_Gate({x: 450, y: 50}, "Conjunction\nand Disjunction"));
-mainCircuit.gates.unshift(new AND_Gate({x: 450, y: 150}));
-mainCircuit.gates.unshift(new OR_Gate({x: 450, y: 250}));
+mainCircuit.gates.unshift(new Lable_Gate({x: 750, y: 50}, "Conjunction\nand Disjunction"));
+mainCircuit.gates.unshift(new AND_Gate({x: 750, y: 150}));
+mainCircuit.gates.unshift(new OR_Gate({x: 750, y: 250}));
 
-mainCircuit.gates.unshift(new Lable_Gate({x: 650, y: 50}, "Alternative denial\nand Joint denial"));
-mainCircuit.gates.unshift(new NAND_Gate({x: 650, y: 150}));
-mainCircuit.gates.unshift(new NOR_Gate({x: 650, y: 250}));
+mainCircuit.gates.unshift(new Lable_Gate({x: 950, y: 50}, "Alternative denial\nand Joint denial"));
+mainCircuit.gates.unshift(new NAND_Gate({x: 950, y: 150}));
+mainCircuit.gates.unshift(new NOR_Gate({x: 950, y: 250}));
 
-mainCircuit.gates.unshift(new Lable_Gate({x: 900, y: 50}, "Exclusive or\nand Biconditional"));
-mainCircuit.gates.unshift(new XOR_Gate({x: 900, y: 150}));
-mainCircuit.gates.unshift(new XNOR_Gate({x: 900, y: 250}));
+mainCircuit.gates.unshift(new Lable_Gate({x: 1200, y: 50}, "Exclusive or\nand Biconditional"));
+mainCircuit.gates.unshift(new XOR_Gate({x: 1200, y: 150}));
+mainCircuit.gates.unshift(new XNOR_Gate({x: 1200, y: 250}));
 
-mainCircuit.gates.unshift(new Lable_Gate({x: 1150, y: 50}, "Outputs"));
-mainCircuit.gates.unshift(new Lamp_Gate({x: 1150, y: 150}));
-mainCircuit.gates.unshift(new Display_Gate({x: 1150, y: 250}));
+mainCircuit.gates.unshift(new Lable_Gate({x: 1450, y: 50}, "Outputs"));
+mainCircuit.gates.unshift(new Lamp_Gate({x: 1450, y: 150}));
+mainCircuit.gates.unshift(new Display_Gate({x: 1450, y: 250}));
 
 
 
@@ -106,4 +106,23 @@ for (let index = 0; index < addButtons.length; index++) {
 let errorContainer = document.getElementById("error-container");
 errorContainer.getElementsByTagName("BUTTON")[0].addEventListener("click", ()=>{
     errorContainer.className = "hidden";
+});
+
+let menu = document.getElementById("menu") as HTMLElement;
+let dragContainer = document.getElementById("drag-container") as HTMLElement;
+let isDraggingMenu: boolean = false;
+
+dragContainer.addEventListener("mousedown", (e)=>{
+    isDraggingMenu = true;
+});
+
+document.addEventListener("mouseup", (e: MouseEvent)=>{
+    isDraggingMenu = false;
+    
+});
+
+document.addEventListener("mousemove", (e: MouseEvent)=>{
+    if(!isDraggingMenu)return;
+    menu.style.left = e.clientX - dragContainer.getBoundingClientRect().width/2 + "px";
+    menu.style.top = e.clientY - dragContainer.getBoundingClientRect().height/2 + "px";
 });
