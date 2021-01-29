3858c8a140a7dafeb6427cac4c3ea35845fc4d19
Marc Beyer <marc.beyer.dev@gmail.com>
2021-01-29 17:21:51 +0100

    Removed 7 segment display from default circuit

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -22,7 +22,7 @@ var activeInfoWindow = null;
 var mainCircuit = new Circuit({ x: 10, y: 10 });
 // Create a new SaveManager
 var saveManager = new SaveManager(mainCircuit);
-mainCircuit.addGate(GATE_TYPE.Segment_Display, { x: 700, y: 300 }, []);
+//mainCircuit.addGate(GATE_TYPE.Segment_Display, {x: 700, y: 300}, []);
 // Add Listener to error-container
 var errorContainer = document.getElementById("error-container-close-btn");
 errorContainer.addEventListener("click", function () {
--- web/src/main.ts
+++ web/src/main.ts
@@ -24,7 +24,7 @@ var mainCircuit = new Circuit({x: 10, y: 10});
 // Create a new SaveManager
 var saveManager = new SaveManager(mainCircuit);
 
-mainCircuit.addGate(GATE_TYPE.Segment_Display, {x: 700, y: 300}, []);
+//mainCircuit.addGate(GATE_TYPE.Segment_Display, {x: 700, y: 300}, []);
 
 // Add Listener to error-container
 let errorContainer = document.getElementById("error-container-close-btn");
